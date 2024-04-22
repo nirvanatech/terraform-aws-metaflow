3d51d4de9f23808a5645c69504b437843762e83b
Sarthak Agrawal <sarthak@nirvanatech.com>
2024-04-22 13:37:50 +0530

    Allow (optional) random suffix to s3 bucket names

--- modules/datastore/locals.tf
+++ modules/datastore/locals.tf
@@ -6,5 +6,15 @@ locals {
   rds_security_group_name = "${var.resource_prefix}rds-security-group${var.resource_suffix}"
 
   # Name of S3 bucket
-  s3_bucket_name = "${var.resource_prefix}s3${var.resource_suffix}"
+  s3_bucket_name_prefix = "${var.resource_prefix}s3${var.resource_suffix}"
+  s3_bucket_name        = var.randomize_s3_name ? "${local.s3_bucket_name_prefix}-${random_string.bucket_suffix.result}" : local.s3_bucket_name_prefix
+}
+
+resource "random_string" "bucket_suffix" {
+  count   = var.randomize_s3_name ? 1 : 0
+  length  = 8
+  lower   = true
+  upper   = false
+  numeric = true
+  special = false
 }
